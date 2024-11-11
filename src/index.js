cd11c5bf4b37b3f65a369aa30c3656530810570f
ThomasHaberkorn <126246580+ThomasHaberkorn@users.noreply.github.com>
2024-11-12 00:19:52 +0100

    navbar bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<Navbar />, document.getElementById("root"));
