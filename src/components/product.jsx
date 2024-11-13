5fecaf037d02fe8be9983b2ce258291e67d09cc2
ThomasHaberkorn <126246580+ThomasHaberkorn@users.noreply.github.com>
2024-11-13 11:56:18 +0100

    navbar bootstrap card import img

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -8,7 +8,7 @@ class Product extends Component {
         <div className="card-body">
           <h5 clasNames="card-title">Card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <a href="/" className="btn btn-primary">Go somewhere</a>
         </div>
       </div>;
     }
